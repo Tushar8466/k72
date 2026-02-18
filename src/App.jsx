89f89f0521cb170c04b1c3cf7dfad3012dd8fe2e
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-02-18 21:54:23 +0530

    feat: Implement animated full-screen navigation using `NavContext` for state management.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 import Navbar from './components/Navigation/Navbar'
+import FullScreenNavbar from './components/Navigation/FullScreenNavbar'
 
 
 const App = () => {
   return (
     <div className='overflow-x-hidden text-white text-3xl'>
       <Navbar />
+      <FullScreenNavbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agence' element={<Agence />} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import NavContext from './context/NavContext'
 import Stairs from './components/common/Stairs'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Stairs />
-      <App />
+      <NavContext>
+        <Stairs>
+          <App />
+        </Stairs>
+      </NavContext>
     </BrowserRouter>
   </StrictMode>,
 )
