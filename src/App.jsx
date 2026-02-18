6040458a4a547e83e41cf379917fd7b7751e7b6a
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-02-18 20:03:10 +0530

    feat: Implement GSAP page transition animation and update asset paths to absolute for video and fonts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import React from 'react'
-import { Route, Routes, Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 function App() {
+
+  useGSAP(() => {
+
+    const tl = gsap.timeline()
+
+    tl.from('.stair', {
+      height: 0,
+      stagger: {
+        amount: -0.3
+      },
+    })
+
+    tl.to('.stair', {
+      y: '100%',
+      stagger: {
+        amount: 0.3
+      }
+    })
+  })
+
+
+
   return (
     <div className='text-white text-3xl'>
-      {/* <Link className='text-blue-500 mr-5 text-sm' to='/'>Home</Link>
-      <Link className='text-blue-500 mr-5 text-sm' to='/agence'>Agence</Link>
-      <Link className='text-blue-500 mr-5 text-sm' to='/projects'>Projects</Link> */}
+      <div className='h-screen w-full fixed z-10 flex'>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+      </div>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/agence' element={<Agence />}></Route>
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay muted loop playsInline src="../public/video.mp4"></video>
+      <video className='h-full w-full object-cover' autoPlay muted loop playsInline src="/video.mp4"></video>
     </div>
   )
 }
