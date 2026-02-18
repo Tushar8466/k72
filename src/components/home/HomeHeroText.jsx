caf9ad31df6026d51a42d34ce68136aeb777f643
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-02-18 19:43:10 +0530

    feat: Add scroll-triggered image animation to Agence page, update video source, and refactor home navigation links.

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -5,7 +5,7 @@ function HomeHeroText() {
   return (
     <div className="font-[font1] pt-5 text-center">
       <div className="text-[10vw] uppercase leading-[8.5vw] flex justify-center items-center">L'étincelle</div>
-      <div className="text-[10vw] uppercase leading-[8.5vw] flex justify-center items-center">qui<div className="h-[7vw] w-[10vw] rounded-full overflow-hidden -mt-5"><Video /></div>génère</div>
+      <div className="text-[10vw] uppercase leading-[8.5vw] flex justify-center items-center">qui<div className="h-[7vw] w-[15vw] rounded-full overflow-hidden -mt-5"><Video /></div>génère</div>
       <div className="text-[10vw] uppercase leading-[8.5vw] flex justify-center items-center">la créativité</div>
     </div>
   );
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay muted loop playsInline src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1771395939~exp=1771399539~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=4087598acd23ad0b24ae77f5aa3c176a6c3707d02592e2e9451535f1c5cd03b0&r=dXMtZWFzdDE%3D"></video>
+      <video className='h-full w-full object-cover' autoPlay muted loop playsInline src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1771414718~exp=1771418318~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=66181304376b57e09b180afb00b83bad752ad7f3588b021e7f320c12fb6f4682&r=dXMtZWFzdDE%3D"></video>
     </div>
   )
 }
