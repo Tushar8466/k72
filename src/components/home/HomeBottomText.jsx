745c2c5400f41eae95e989e2278364f1fb8dc15b
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-02-18 12:28:22 +0530

    feat: Implement home page navigation links, adjust hero video dimensions, and refine main content layout.

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomeBottomText() {
   return (
-    <div>
-      
+    <div className='font-[font2] flex items-center justify-center gap-4 pb-10'>
+      <Link className='text-[6.5vw] border-[5px] border-white rounded-full px-5 py-3 pt-6 uppercase leading-[6vw] transition-all duration-300 hover:text-[#bbf628] hover:border-[#bbf628] cursor-pointer'>Projets</Link>
+      <Link className='text-[6.5vw] border-[5px] border-white rounded-full px-5 py-3 pt-6 uppercase leading-[6vw] transition-all duration-300 hover:text-[#bbf628] hover:border-[#bbf628] cursor-pointer'>Agence</Link>
     </div>
   )
 }
