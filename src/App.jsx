9d32d5c4ed75dd2736ab8512679ecb3b2d727484
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-02-18 21:24:21 +0530

    feat: Implement navigation components and a GSAP-driven page transition system.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-
-function App() {
-
-  useGSAP(() => {
-
-    const tl = gsap.timeline()
-
-    tl.from('.stair', {
-      height: 0,
-      stagger: {
-        amount: -0.3
-      },
-    })
-
-    tl.to('.stair', {
-      y: '100%',
-      stagger: {
-        amount: 0.3
-      }
-    })
-  })
+import Navbar from './components/Navigation/Navbar'
 
 
-
+const App = () => {
   return (
-    <div className='text-white text-3xl'>
-      <div className='h-screen w-full fixed z-10 flex'>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-      </div>
+    <div className='overflow-x-hidden text-white text-3xl'>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/agence' element={<Agence />}></Route>
-        <Route path='/projects' element={<Projects />}></Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/projects' element={<Projects />} />
       </Routes>
     </div>
   )
---
+++ src/components/Navigation/FullScreenNavbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FullScreenNavbar() {
+  return (
+    <div>FullScreenNavbar</div>
+  )
+}
+
+export default FullScreenNavbar
